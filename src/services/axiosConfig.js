b24e91b6da094acd403640227db5485b8f6e42fa
subarnashekhar <subarnashekhar@gmail.com>
2026-04-29 23:12:25 -0500

    Added All Files

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,6 +1,9 @@
 
-export const SERVER_BASE_URL = 'http://localhost:9090';
+// export const SERVER_BASE_URL = 'http://localhost:9090';
+//export const SERVER_BASE_URL = 'http://localhost:5001';
 
+//export const SERVER_BASE_URL = 'https://online-foodies-admin-backend-api-env.eba-htvainuu.us-east-1.elasticbeanstalk.com';
+export const SERVER_BASE_URL = 'https://online-foodies-admin.techsmart.click';
 export const API_URL = SERVER_BASE_URL + '/api/foods';
 export const ORDER_API_URL = SERVER_BASE_URL + '/api/orders';
 
